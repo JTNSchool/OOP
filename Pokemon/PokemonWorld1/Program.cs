1632c85b53d7fd580fc31608b547c806c665a039
Jacco Bos <99077364@mydavinci.nl>
2024-04-24 09:16:44 +0200

    Opdracht 1

---
+++ Pokemon/PokemonWorld1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PokemonWorld1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StarterPokemon = new Charmander();
+            //The player gives a name to a charmander.
+            StarterPokemon.ChangeName();
+            //The charmander does its battle cry for ten times.
+            for (int i = 0; i < 10; i++)
+            {
+                StarterPokemon.BattleCry();
+            }
+
+            //Infinite loop until user closes console
+            while (true)
+            {
+                //The player can give a new name to the same charmander.
+                StarterPokemon.ChangeName();
+                //The charmander does its battle cry for ten times.
+                for (int i = 0; i < 10; i++)
+                {
+                    StarterPokemon.BattleCry();
+                }
+            }
+
+
+
+        }
+    }
+}
